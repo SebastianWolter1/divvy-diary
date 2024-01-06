5fabc6de656b6d2e57329109f6b15e910f006667
Seb <seb@techtakel.berlin>
2024-01-06 14:08:36 +0100

    adds loading spinner and error handling

---
+++ app/auth/register/error.js
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="h-screen flex flex-col items-center mt-24 px-2">
+      <div className="text-left">
+        <h2 className="text-4xl mb-2">
+          <span className="block font-semibold">Etwas ist schief gelaufen!</span>
+          <span className="block text-orange-700">Versuch es nochmal</span>
+        </h2>
+        <button 
+          className="bg-orange-600 hover:bg-orange-700 text-gray-300 dark:text-white font-bold py-2 px-4 rounded"
+          onClick={() => reset()}
+        >
+          Try again
+        </button>
+      </div>
+    </div>
+  );
+}
