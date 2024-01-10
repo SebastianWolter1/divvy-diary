35fe51c32f34840c8490aa6ce345be41f23c94cd
Seb <seb@techtakel.berlin>
2024-01-10 12:52:59 +0100

    adds new DB and refactors addAlarm

--- app/api/addAlarm/route.js
+++ app/api/addAlarm/route.js
@@ -6,23 +6,19 @@ export async function POST(request) {
     const body = await request.json();
     const { isin, price, userId } = body;
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}${body.isin}`
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${body.isin}`);
     if (!res.ok) {
       return null;
     }
     const result = await res.json();
     const stockPrice = result.price.toFixed(2);
 
-    let initialPrice;
-    if (stockPrice > body.price) {
-      initialPrice = 'lower';
-    } else if (stockPrice < body.price) {
-      initialPrice = 'higher';
-    } else {
-      initialPrice = 'equal';
-    }
+  let initialPrice =
+   stockPrice > body.price.replace(",", ".")
+        ? "lower"
+        : stockPrice < body.price.replace(",", ".")
+        ? "higher"
+        : "equal";
 
     const priceAlarm = await prisma.priceAlarms.create({
       data: { isin, price, userId, initialPrice },
